84302d177f37ea67fe9399d3cdc6045e98bc600b
yavuzefe <yavuzefe@gmail.com>
2020-12-13 20:18:21 +0100

    kelle pacaci programi yazildi

---
+++ kellePAcacaiNecipUsta.js
@@ -0,0 +1,12 @@
+// ciftlikteki hayvanlari array icerisinde tutariz
+const hayvanlar = ["inek", "tavuk", "domuz", "inek", "koyun", "domuz", "tavuk", "koyun", "inek", "tavuk", "koyun"];
+// hayvanlarin bacakari icin degisken tanimlayip ilk degeri 0 atariz.
+bacakSayisi = 0;
+for (let i = 0; i < hayvanlar.length; i++) { // arraydeki hayvanlari dongu ile sirayla tek tek cagiririz
+// dizindeki siradaki hayva kelle paca icin uygun hayvanlardan mi? yani inek veya koyun mu?
+    if (hayvanlar[i] == "inek" || hayvanlar[i] == "koyun") {
+        bacakSayisi += 4; // eger siradaki hayvan kelle paca icin uygunsa kelle paca sayisini bacaksayisi kadar yani 4 arttiririz.
+    }
+}
+
+console.log(bacakSayisi);
